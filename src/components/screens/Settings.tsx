abb6a9052c23357013fb077f0504e2e7a3fbecbe
Tyler Rockwood <rockwotj@gmail.com>
2023-02-21 13:47:05 -0600

    Add race date setting

--- src/components/screens/Settings.tsx
+++ src/components/screens/Settings.tsx
@@ -1,4 +1,6 @@
+import {useLiveQuery} from 'dexie-react-hooks';
 import {useLocation} from 'wouter';
+import {db} from '../../lib/db';
 import {queryParams} from '../../lib/url';
 import {CloseIcon} from '../common/Icon';
 import classes from './settings.module.css';
@@ -10,10 +12,18 @@ export function Settings(): JSX.Element {
     const prev = queryParams().get("prev");
     navigate(prev ? decodeURIComponent(prev) : "/");
   };
+  // TODO: Handle flickering loading
+  const raceDate = useLiveQuery(() => db.settings.get("race-date"), [], null);
   return (<div className={classes.container}>
     <CloseIcon onClick={handleClose} />
-    <div>
-      Settings go here
+    <div className={classes.section}>
+      <h3>Race Date</h3>
+      <input 
+        type="date"
+        className={classes.dateInput}
+        value={raceDate?.value || ""} 
+        onChange={(e) => db.settings.put({id: 'race-date', value: e.target.value})}
+      />
     </div>
   </div>);
 }
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -13,10 +13,18 @@ interface CompletedWorkout {
   readonly time: number;
 }
 
+interface RaceDateSetting {
+  readonly id: 'race-date';
+  value: string;
+}
+
+export type Setting = RaceDateSetting;
+
 
 export class AppDatabase extends Dexie {
   permissions!: Table<Permission, Permission['id']>; 
   completedWorkouts!: Table<CompletedWorkout, CompletedWorkout['id']>; 
+  settings!: Table<Setting, Setting['id']>; 
 
   constructor() {
     super('app_database');
@@ -26,6 +34,11 @@ export class AppDatabase extends Dexie {
     this.version(2).stores({
       permissions: 'id',
       completedWorkouts: '++id'
+    });
+    this.version(3).stores({
+      permissions: 'id',
+      completedWorkouts: '++id',
+      settings: 'id',
     });
   }
 }
